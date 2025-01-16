9da59a7352bb9efc5794e34ab82d0c96ab7d9ba7
oriionn <contact@oriondev.fr>
2025-01-16 22:10:10 +0100

    Add standalone installation instruction

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -18,7 +18,10 @@ export default async function Page(props: {
   const MDX = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage toc={page.data.toc} full={page.data.full} tableOfContent={{
+      style: 'normal',
+      single: false,
+    }}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>

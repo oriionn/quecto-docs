ce0427548cb9401466a28319c4a99cc7ca1c8ad2
oriionn <contact@oriondev.fr>
2025-01-17 13:37:47 +0100

    Add some remark plugins

--- source.config.ts
+++ source.config.ts
@@ -1,8 +1,11 @@
 import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
-import {rehypeToc, remarkHeading} from "fumadocs-core/mdx-plugins";
+import {rehypeCode, rehypeToc, remarkHeading} from "fumadocs-core/mdx-plugins";
 
 export const { docs, meta } = defineDocs({
   dir: 'content/docs',
 });
 
-export default defineConfig();
+export default defineConfig({
+  remarkPlugins: [remarkHeading, rehypeCode, rehypeToc],
+  lastModifiedTime: 'git'
+});

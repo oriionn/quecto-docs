f1f3628a3cf50d9686509037b19579dcb2142f91
Orion <contact@oriondev.fr>
2025-01-20 11:37:20 +0100

    Add French API Documentation

--- scripts/generate-docs.mjs
+++ scripts/generate-docs.mjs
@@ -1,5 +1,5 @@
 import * as OpenAPI from 'fumadocs-openapi';
-import { readdirSync, rmSync } from "fs";
+import { readdirSync, rmSync, mkdir, mkdirSync, cpSync, rmdirSync, existsSync } from "fs";
 import {join} from "path";
 const out = './content/docs/api';
 
@@ -10,9 +10,26 @@ readdirSync(out).forEach(file => {
   }
 });
 
+if (existsSync(`${out}/fr`)) rmdirSync(`${out}/fr`);
+
 void OpenAPI.generateFiles({
   // input files
   input: ['./openapi.json'],
   output: out,
   per: "operation"
 });
+
+mkdirSync(`${out}/fr`);
+void OpenAPI.generateFiles({
+  // input files
+  input: ['./openapi.fr.json'],
+  output: `${out}/fr`,
+  per: "operation",
+}).then(() => {
+  readdirSync(`${out}/fr`).forEach(file => {
+    let name = file.slice(0, -4)
+    cpSync(`${out}/fr/${file}`, `${out}/${name}.fr.mdx`);
+  })
+
+  rmSync(`${out}/fr`, {recursive: true, force: true});
+})
